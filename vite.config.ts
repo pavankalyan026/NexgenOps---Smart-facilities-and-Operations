2934475f997922fa189cf2994ad980b69456eb69
Pavankalyan koneti <124763316+pavankalyan026@users.noreply.github.com>
2026-09-08 12:36:34 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/NexgenOps---Smart-facilities-and-Operations/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
